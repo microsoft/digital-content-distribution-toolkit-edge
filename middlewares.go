39d3a4f8e01360025a4c561f1818149dbbc4d2e8
Abhishek Vijaya Kumar <t-abvij@microsoft.com>
2020-07-02 20:54:49 +0530

    Authorization header change

--- middlewares.go
+++ middlewares.go
@@ -1,30 +1,35 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	ini "gopkg.in/ini.v1"
 
 	jwt "github.com/dgrijalva/jwt-go"
 )
-
 
 type Claims struct {
 	Userid int `json:"user_id"`
 	jwt.StandardClaims
 }
 
+var middlewareCfg *ini.File = nil
+
 // generate ssh keys using keygen as described in: https://blog.miguelgrinberg.com/post/json-web-tokens-with-public-key-signatures
 // put publine key should be in pem format, to convert .pub file generate above into .pem file use https://unix.stackexchange.com/a/310350/332228
 func verifyToken(token string) (*Claims, error) {
 	keys := km.GetKeyList()
-
-    for i, key := range keys {
+	fmt.Println("Length of keys: ", len(keys))
+	for i, key := range keys {
 		claims := &Claims{}
 		tkn, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
 			return key, nil
 		})
-		
+
 		// if err != nil {
 		// 	return claims, err
 		// }
@@ -34,6 +39,7 @@ func verifyToken(token string) (*Claims, error) {
 		if err == nil && tkn.Valid {
 			return claims, nil
 		}
+		fmt.Println(err.Error())
 		fmt.Println(i)
 	}
 
@@ -41,11 +47,21 @@ func verifyToken(token string) (*Claims, error) {
 }
 
 func AuthRequiredMiddleware(c *gin.Context) {
-	authToken := c.GetHeader("X-Auth-Token")
-
+	if middlewareCfg == nil {
+		cfg, err := ini.Load("hub_config.ini")
+		if err != nil {
+			fmt.Println("Failed to read config file: %v in Auth middleware", err)
+			os.Exit(1)
+		}
+		middlewareCfg = cfg
+	}
+	authToken := c.GetHeader("Authorization")
+	fmt.Println("Received token is: ", authToken)
+	authToken = strings.Replace(authToken, "Bearer ", "", 1)
+	fmt.Println("Relevant part: ", authToken)
 	fmt.Println("checking if valid token...")
 	claims, err := verifyToken(authToken)
-    if err != nil && err.Error() == "Token is invalid" {
+	if err != nil && err.Error() == "Token is invalid" {
 		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
 			"status": "error",
 			"data": gin.H{
@@ -53,10 +69,10 @@ func AuthRequiredMiddleware(c *gin.Context) {
 			},
 		})
 		return
-    }
+	}
 
 	c.Set("userId", claims.Userid)
-
+	c.Header("hubId", middlewareCfg.Section("DEVICE_INFO").Key("DEVICE_NAME").String())
 	// Pass on to the next-in-chain
 	c.Next()
 }
